8a4f334429ddfefd98659efa06e8d4504da56124
Alireza Gholami <68914369+AlirezaGholamiii@users.noreply.github.com>
2021-10-26 19:40:03 -0400

    Android Studio

---
+++ app/src/main/java/com/example/prjfirebasealireza/model/Person.java
@@ -0,0 +1,52 @@
+package com.example.prjfirebasealireza.model;
+
+import androidx.annotation.NonNull;
+
+public class Person {
+
+
+    //attributes must be same as firebase keys
+    private int id;
+    private String name;
+    private int age;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person() {
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "id: "+id+" Name: "+name+" Age: "+age;
+    }
+}
